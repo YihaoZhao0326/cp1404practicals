0475377cad3dedf887bf28cc9428b9cd2222fb09
YihaoZhao0326 <yihao.zhao@my.jcu.edu.au>
2024-06-02 21:20:38 +0800

    add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,15 +2,18 @@ PASSWORD_LENGTH = 6
 
 
 def main():
+    """get a valid password and convert it to asterisks"""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """convert password to asterisks"""
     print("*" * len(password))
 
 
 def get_password():
+    """get a valid password which length is greater than PASSWORD_LENGTH"""
     password = input("password: ")
     while len(password) < PASSWORD_LENGTH:
         print(f"Your password shouldn't less than {PASSWORD_LENGTH}")
