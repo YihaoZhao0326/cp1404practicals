161547a2d70b2ce7f74bcc8ffa60e4db150985d3
YihaoZhao0326 <yihao.zhao@my.jcu.edu.au>
2024-06-09 21:17:52 +0800

    random numbers complete

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+"""
+What did you see on line 1?
+7, 8, 17
+smallest:5 largest:20
+
+What did you see on line 2?
+3, 9, 3
+smallest: 3 largest:9
+4 cannot be produced
+
+What did you see on line 3?
+3.1055867098438634, 4.937902149069454, 5.10916510940422
+smallest: 2.500000...
+largest: 5.500000...
+"""
+import random
+print(random.uniform(1, 100))
