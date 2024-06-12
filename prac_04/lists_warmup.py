4e311b47e18add49c8f84b75a3d3626ed0721b8c
YihaoZhao0326 <yihao.zhao@my.jcu.edu.au>
2024-06-12 16:51:20 +0800

    Warm-Up complete

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+print(numbers[0])  # 3
+print(numbers[-1])  # 2
+print(numbers[3])  # 1
+print(numbers[:-1])  # 3 1 4 1 5 9
+print(numbers[3:4])  # 1
+print(5 in numbers)  # True
+print(7 in numbers)  # False
+print("3" in numbers)  # False
+print(numbers + [6, 5, 3])  # 3 1 4 1 5 9 2 6 5 3
+
+# Q1
+numbers[0] = "ten"
+# Q2
+numbers[-1] = 1
+# Q3
+print(numbers[2:])
+# Q4
+print(9 in numbers)
